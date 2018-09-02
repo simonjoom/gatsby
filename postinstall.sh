c327d82b0b05a76f5c8e14f82886936c7c881b7f
simon <simon@skiscool.com>
2018-09-02 17:19:26 +0200

    reactnativeweb hacked

--- postinstall.sh
+++ postinstall.sh
@@ -8,3 +8,4 @@ cp -Rf ./react-native-vector-icons ./node_modules/
 cp -Rf ./utils ./node_modules/gatsby/dist/
 cp -Rf ./@reach ./node_modules/
 cp -Rf ./react-dev-utils ./node_modules/
+cp -Rf ./react-native-web ./node_modules/
