09e742fea3d5d13e0a5d5e83d213bb00e1e0e521
simon <simon@skiscool.com>
2018-09-05 13:44:27 +0200

    I forgot this

--- postinstall.sh
+++ postinstall.sh
@@ -2,14 +2,14 @@
 # deep copy script
 export NOYARNPOSTINSTALL="yes"
 
-cp -Rf ./gatsby-plugin-react-native-web ./node_modules/
-cp -Rf ./cache-dir ./node_modules/gatsby/
-cp -Rf ./react-native-vector-icons ./node_modules/
-cp -Rf ./utils ./node_modules/gatsby/dist/ 
-cp -Rf ./@reach ./node_modules/
-cp -Rf ./react-dev-utils ./node_modules/
-cp -Rf ./react-native-web ./node_modules/
-cp -Rf ./react-navigation-stack ./node_modules/ 
-cp -Rf ./gatsby ./node_modules/ 
-cp -Rf ./gatsby-mdx ./node_modules/ 
-cp -Rf ./htmltojsx ./node_modules/ 
+cp -Rf ./packhack/gatsby-plugin-react-native-web ./node_modules/
+cp -Rf ./packhack/cache-dir ./node_modules/gatsby/
+cp -Rf ./packhack/react-native-vector-icons ./node_modules/
+cp -Rf ./packhack/utils ./node_modules/gatsby/dist/ 
+cp -Rf ./packhack/@reach ./node_modules/
+cp -Rf ./packhack/react-dev-utils ./node_modules/
+cp -Rf ./packhack/react-native-web ./node_modules/
+cp -Rf ./packhack/react-navigation-stack ./node_modules/ 
+cp -Rf ./packhack/gatsby ./node_modules/ 
+cp -Rf ./packhack/gatsby-mdx ./node_modules/ 
+cp -Rf ./packhack/htmltojsx ./node_modules/ 
